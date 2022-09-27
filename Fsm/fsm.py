33f6dc5c859d36c7dbf1723cc3c8c3d4a6310119
Ale <alelei94@yahoo.it>
2022-09-27 20:01:40 +0200

    Add FSM

---
+++ Fsm/fsm.py
@@ -0,0 +1,52 @@
+import Buttons.buttons as Buttons
+import Patterns.patterns as Patterns
+
+# FSM states
+FSM_INIT = 0
+FSM_RUN = 1
+FSM_STOP = 2
+
+patterns = [ Patterns.colorwave, Patterns.fireflies, Patterns.rainbow, Patterns.smoothRainbow, Patterns.setRange ]
+patternCount = len(patterns)
+
+__fsmState__ = FSM_INIT
+__patternIdx__ = 0
+
+def setState(state):
+    global __fsmState__
+
+    __fsmState__ = state
+
+def getState(state):
+    global __fsmState__
+
+    return __fsmState__
+
+def patternFW():
+    global __patternIdx__, patterns
+
+    if(__patternIdx__ == patternCount - 1):
+        __patternIdx__ = 0
+    else:
+        __patternIdx__ = __patternIdx__ + 1
+
+def patternBW():
+    global __patternIdx__, patterns
+
+    if(__patternIdx__ == 0):
+        __patternIdx__ = patternCount - 1
+    else:
+        __patternIdx__ = __patternIdx__ - 1
+
+def run(strip, numpix):
+    global FSM_INIT, FSM_RUN, FSM_STOP
+    global patterns, __patternIdx__
+    global __fsmState__
+
+    if(__fsmState__ == FSM_INIT):
+        Buttons.Init()
+        __fsmState__ = FSM_RUN
+
+    elif(__fsmState__ == FSM_RUN):
+        patterns[__patternIdx__](strip, numpix)
+
