5dd686692696c03e1987a35ec1b77f8a5163ca55
Ale <alelei94@yahoo.it>
2022-09-30 20:09:32 +0200

    Main to call Fsm.init() and set strip brighness

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from Neopixel_lib.neopixel import Neopixel
+from Patterns.patterns import __brightness__
 import Fsm.fsm as Fsm
 import time
 
 numpix = 300
 strip = Neopixel(numpix, 1, 1, "GRB")
+strip.brightness(__brightness__)
+
+Fsm.init(strip, numpix)
 
 while True:
-    Fsm.run(strip, numpix)
+    Fsm.run()
 
     time.sleep(0.1)
