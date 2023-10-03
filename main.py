b5b1c8b088abb24b6163ddfa848639a9987f5ad9
Ale <alelei94@yahoo.it>
2023-10-03 15:02:08 +0200

    Updates main code

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from Neopixel_lib import neopixel
-import Buttons.buttons as Buttons
-import Patterns.patterns as Patterns
-import Fsm.fsm as Fsm
+from LedController.LedController import LedController
 import time
 
-numpix = 120
-strip = neopixel.Neopixel(numpix, 1, 15)
+btn1Pin = 20 # Pattern FW
+btn2Pin = 18 # Pattern BW
+btn3Pin = 21 # Brightness UP - Party mode if pressed at boot
+btn4Pin = 19 # Brightness DOWN - Mic pattern if pressed at boot
+numLeds = 120 # Number of LEDs in the strip
 
-Fsm.init(strip, numpix)
+Leds = LedController(numLeds, btn1Pin, btn2Pin, btn3Pin, btn4Pin)
 
+Leds.init()
 
 while True:
-    Fsm.run(Buttons.checkMode())
+    Leds.run()
 
     time.sleep(0.1)
