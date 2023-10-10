220a71efb5806a0d93d79d83a29266ff4bcda19f
Ale <alelei94@yahoo.it>
2023-10-10 14:48:42 +0200

    Adds Setting for simpleColor index + make sure to return int

--- Settings/settings.py
+++ Settings/settings.py
@@ -3,25 +3,35 @@ import time
 import os
 
 #Settings files name's list
-__settingsList__ = [ "bright.txt" ]
+__settingsList__ = [ "bright.txt", "simpleColorIdx.txt" ]
 
 os.chdir("/")
 
 def readSetting(idx=0):
     global __settingsList__
-    file = open(__settingsList__[idx])
 
+    # Check index is valid
+    if (idx > len(__settingsList__)):
+        print("Invalid setting index ", idx, " max is ", len(__settingsList__))
+        return 0
+
+    file = open(__settingsList__[idx])
     value = file.read()
+
+    # Check what was read from the file
+    # and make sure it's properly casted to integer
+    if (value == ""):
+        value = 0
+    else:
+        value = int(value)
+
     #print("Read", value)
     file.close()
 
-    return int(value)
+    return value
 
 def writeSetting(value, idx=0):
-    file = open(__settingsList__[idx], "w")
+    file = open(__settingsList__[idx], 'w')
 
     file.write(str(value))
-    #print("Written", value)
     file.close()
-
-
