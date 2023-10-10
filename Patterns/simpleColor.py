848678445c71d96fae561b43491a25bd5e959393
Ale <alelei94@yahoo.it>
2023-10-10 14:49:12 +0200

    Updates simpleColor pattern to load index from Settings

--- Patterns/simpleColor.py
+++ Patterns/simpleColor.py
@@ -1,23 +1,7 @@
 from Patterns.pattern import Pattern
+import Settings.settings as Settings
 from Color import color as Colors
 import time, random
-
-__color = 0
-
-def simpleColorInit(strip, numpix, colorsList):
-    global __color
-
-    __color = colorsList[random.randint(0, len(colors)-1)]
-
-    print("Simple color initialized with R:", __color[0], " G:", __color[1], " B:", __color[2])
-
-def simpleColor(strip, numpix, colorsList):
-    print("Simple color running")
-
-    strip.fill(__color)
-    strip.show()
-
-    time.sleep(0.01)
 
 colors = [  Colors.red.getRGB(),
             Colors.orange.getRGB(),
@@ -31,5 +15,30 @@ colors = [  Colors.red.getRGB(),
             Colors.waterGreen.getRGB(),
             Colors.acidGreen.getRGB(),
             Colors.darkOrange.getRGB() ]
+
+__color = 0
+
+def loadIndex():
+    index = Settings.readSetting(1)
+    index = (index + 1) % len(colors)
+
+    Settings.writeSetting(index, 1)
+
+    return index
+
+def simpleColorInit(strip, numpix, colorsList):
+    global __color
+
+    __color = colorsList[loadIndex()]
+
+    print("Simple color initialized with R:", __color[0], " G:", __color[1], " B:", __color[2])
+
+def simpleColor(strip, numpix, colorsList):
+    print("Simple color running")
+
+    strip.fill(__color)
+    strip.show()
+
+    time.sleep(0.01)
 
 simpleColorPattern = Pattern(simpleColorInit, simpleColor, colors)
